ac36eea47ec4f68ba5abe4c1627c5082e7d40ec1
thenujad <dulwanat@gmail.com>
2024-03-02 23:16:14 +0530

    next updated commit

--- src/main/java/com/example/bigstarcollectible/controllers/HomeController.java
+++ src/main/java/com/example/bigstarcollectible/controllers/HomeController.java
@@ -2,12 +2,21 @@ package com.example.bigstarcollectible.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class HomeController {
 
+    @GetMapping(path = "/home")
     public String displayHome(Model model)
     {
        return "index";
+    }
+
+    @GetMapping("/getCharacter/{charname}")
+    public String getCharacter(@PathVariable("charname") String charName)
+    {
+        return "/characters/"+charName;
     }
 }
